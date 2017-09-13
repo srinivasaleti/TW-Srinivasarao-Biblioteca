9a6ea8fc0f5890793ef3e3a30009966649bef634
srinivasarao <srinivra@thoughtworks.com>
2017-09-13 11:03:06 +0530

    Add a representation for library item
    * Presently biblioteca holds only books, but now it should hold movies also.
    If we treat books, movies separatly then we will get lot of duplication.If
    we terate book and movie as a library item and if biblioteca holds library items
    then ultimatly we can reduce duplication.

---
+++ src/com/tw/model/LibraryItem.java
@@ -0,0 +1,10 @@
+package com.tw.model;
+
+//Represents an item in biblioteca
+public interface LibraryItem {
+
+    public String stringRepresentation();
+
+    public boolean isSameName(String name);
+
+}
